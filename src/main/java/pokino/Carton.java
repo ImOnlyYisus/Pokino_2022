e797eb5eee3a1e43aec4911cd850c3e980960160
hind <hindsamiri2@gmail.com>
2022-03-22 22:01:57 +0100

    añadir  metodo isCartaExist()

--- src/main/java/pokino/Carton.java
+++ src/main/java/pokino/Carton.java
@@ -22,4 +22,22 @@ public class Carton {
     public Carta[][] getCarton() {
         return carton;
     }
+
+    //method looking if a cart exist in a carton or not
+    public boolean isCartaExist(Carta carta){
+        boolean exist=false;
+        for (int i = 0; i <carton.length; i++) {
+            for (int j = 0; j < carton[i].length; j++) {
+                if(carton[i][j].equals(carta)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    //method check premio centro
+    public void checkPremioCentro(){
+
+    }
 }
