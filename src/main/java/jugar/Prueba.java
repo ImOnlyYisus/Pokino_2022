d4ad29ee3fcbf0eccf0f7ad1bf10b5bc5d20164e
hind <hindsamiri2@gmail.com>
2022-04-19 22:34:02 +0200

    prueba de estampa

--- src/main/java/jugar/Prueba.java
+++ src/main/java/jugar/Prueba.java
@@ -10,8 +10,10 @@ public class Prueba {
         Carton carton = new Carton();
         //imprimir carton
         carton.imprimirCarton();
-
+        System.out.println(carton.coincidenciaLinea());
+        System.out.println(carton.coincidenciaVertical());
         System.out.println(carton.checkPremioPokino());
+        System.out.println(carton.recorrerMap());
 //        carton.mostrar();
 //        carton.recorrerMap();
 
