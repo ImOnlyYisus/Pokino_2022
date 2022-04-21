20a1604d7057478fb65c19ba83fd176e5b050660
yisusturtle <yisusturtle@gmail.com>
2022-04-21 15:46:19 +0200

    Arreglo de mirar coincidencia lineal

--- src/main/java/pokino/Carton.java
+++ src/main/java/pokino/Carton.java
@@ -9,11 +9,11 @@ public class Carton {
     public Carton() {
 //        temporar
 //      carton = new Carta[][]{
-//                {new Carta(Simbolo.COPAS, 1, false), new Carta(Simbolo.COPAS, 0, true), new Carta(Simbolo.SOTA, 0, false), new Carta(Simbolo.SOTA, 9, true), new Carta(Simbolo.OROS, 0, true)},
-//               {new Carta(Simbolo.SOTA, 1, true), new Carta(Simbolo.SOTA, 2, true), new Carta(Simbolo.COPAS, 3, false), new Carta(Simbolo.SOTA, 3, true), new Carta(Simbolo.OROS, 9, true)},
-//               {new Carta(Simbolo.OROS, 1, false), new Carta(Simbolo.OROS, 3, false), new Carta(Simbolo.COPAS, 4, false), new Carta(Simbolo.SOTA, 5, false), new Carta(Simbolo.OROS, 3, false)},
-//              {new Carta(Simbolo.COPAS, 2, true), new Carta(Simbolo.COPAS, 4, true), new Carta(Simbolo.COPAS, 6, false), new Carta(Simbolo.SOTA, 8, false), new Carta(Simbolo.OROS, 4, false)},
-//               {new Carta(Simbolo.ESPADAS, 1, true), new Carta(Simbolo.ESPADAS, 5, true), new Carta(Simbolo.COPAS, 8, false), new Carta(Simbolo.SOTA, 6, false), new Carta(Simbolo.OROS, 5, false)}
+//                {new Carta(Simbolo.COPAS, 1, true), new Carta(Simbolo.COPAS, 0, false), new Carta(Simbolo.SOTA, 0, true), new Carta(Simbolo.SOTA, 9, true), new Carta(Simbolo.OROS, 0, false)},
+//               {new Carta(Simbolo.SOTA, 1, true), new Carta(Simbolo.SOTA, 2, true), new Carta(Simbolo.COPAS, 3, true), new Carta(Simbolo.SOTA, 3, true), new Carta(Simbolo.OROS, 9, false)},
+//               {new Carta(Simbolo.OROS, 1, false), new Carta(Simbolo.OROS, 3, false), new Carta(Simbolo.COPAS, 4, true), new Carta(Simbolo.SOTA, 5, false), new Carta(Simbolo.OROS, 3, false)},
+//              {new Carta(Simbolo.COPAS, 2, true), new Carta(Simbolo.COPAS, 4, true), new Carta(Simbolo.COPAS, 6, false), new Carta(Simbolo.SOTA, 8, false), new Carta(Simbolo.OROS, 4, true)},
+//               {new Carta(Simbolo.ESPADAS, 1, false), new Carta(Simbolo.ESPADAS, 5, false), new Carta(Simbolo.COPAS, 8, false), new Carta(Simbolo.SOTA, 6, true), new Carta(Simbolo.OROS, 5, false)}
 //      };
         this.map = rellenarElMap();
     }
@@ -89,24 +89,20 @@ public class Carton {
     //recorrer horizontal carton and check if any linea have marked cards
     public boolean coincidenciaLinea() {
         boolean allSame = true;
-        for (int i = 0; i < carton.length; i++) {
-            for (int j = 0; j < carton[i].length; j++) {
-                for (int k = i + 1; k < carton.length; k++) {
-                    if (!((carton[i][j].isEstaMarcado()) && (carton[i][k].isEstaMarcado()))) {
-                        allSame = false;
-                        break;
-                    } else {
-                        allSame = true;
-                    }
-                }
-                if (!allSame) {
+
+        for (int i = 0; i <carton.length ; i++) {
+            allSame=true;
+            for (int j = 0; j <carton[i].length ; j++) {
+                if(!carton[i][j].isEstaMarcado()){
+                    allSame=false;
                     break;
-                }else {
-                    return allSame;
                 }
             }
+            if(allSame){
+                return true;
+            }
         }
-        return allSame;
+        return false;
     }
 
     public boolean coincidenciaDiagonal() {
